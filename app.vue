461ccbe2f555083f58722239cde5d0d54781980a
Barbapapazes <e.soubiran25@gmail.com>
2023-02-16 09:10:59 +0100

    feat: add image

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
-  </div>
+    <h1>Test de Nuxt Image on Netlify</h1>
+    <NuxtImg src="/photo.png" format="webp" sizes="sm:100px md:200px lg:400px" />
+</div>
 </template>
